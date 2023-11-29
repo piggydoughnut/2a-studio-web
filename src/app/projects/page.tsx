19f8d624178304254ddd665ffb24a483f3d4e632
piggydoughnut <piggydoughnut@gmail.com>
2023-11-29 21:15:27 +0800

    filtering projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import CustomLink from "@/components/CustomLink";
 import Image from "next/image";
 import PageHeader from "@/components/PageHeader";
 import Par from "@/components/Par";
+
+const Types = {
+  Commercial: "Commercial",
+  Residential: "Residential",
+  School: "School",
+};
 
 const Tile = ({
   image,
@@ -18,6 +28,85 @@ const Tile = ({
   </div>
 );
 
+const projectsList = [
+  {
+    id: "#batch",
+    title: "Grahamstown Florist",
+    type: Types.Commercial,
+    image: "/projects/Project_other2.png",
+  },
+  {
+    id: "#batch",
+    title: "Batch in the Coromandel",
+    type: Types.Residential,
+    image: "/projects/Project_08.png",
+  },
+  {
+    id: "#batch",
+    title: "Coromandel Town School",
+    type: Types.School,
+    image: "/projects/Project_12.png",
+  },
+  {
+    id: "#batch",
+    title: "43 Brown street, Thames",
+    type: Types.Residential,
+    image: "/projects/Project_14.png",
+  },
+  {
+    id: "#batch",
+    title: "Beach house",
+    type: Types.Residential,
+    image: "/projects/Project_15.png",
+  },
+
+  {
+    id: "#batch",
+    title: "Taranaki school",
+    type: Types.School,
+    image: "/projects/Project_24.png",
+  },
+
+  {
+    id: "#batch",
+    title: "Auckland beach house",
+    type: Types.Commercial,
+    image: "/projects/Project_30.png",
+  },
+
+  {
+    id: "#batch",
+    title: "Grahamstown Florist",
+    type: Types.Residential,
+    image: "/projects/Project_39.png",
+  },
+  {
+    id: "#batch",
+    title: "School in the Coromandel",
+    type: Types.School,
+    image: "/projects/Project_129.png",
+  },
+  {
+    id: "#batch",
+    title: "School in Waihi",
+    type: Types.Residential,
+    image: "/projects/Project_other.png",
+  },
+  {
+    id: "#batch",
+    title: "Batch in the Coromandel",
+    type: Types.Residential,
+    image: "/projects/Project_other3.png",
+  },
+
+  {
+    id: "#batch",
+    title: "Waihi beach school",
+    type: Types.Commercial,
+    image: "/projects/Project_other4.png",
+  },
+];
+
 const ProjectTitle = ({ title }: { title: string }) => {
   return (
     <div className="flex flex-col">
@@ -29,117 +118,46 @@ const ProjectTitle = ({ title }: { title: string }) => {
   );
 };
 export default function Home() {
+  const [projects, setProjects] = useState(projectsList);
+  const [selectedType, setSelectedType] = useState("");
+
+  useEffect(() => {
+    setProjects(projectsList.filter((p) => p.type === selectedType));
+    if (!selectedType) {
+      setProjects(projectsList);
+    }
+  }, [selectedType]);
   return (
     <div className="flex  ">
       <div className="flex flex-col gap-10 flex-1">
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <p className="opacity-60">projects by name</p>
-          {[
-            {
-              title: "Grahamstown Florist",
-              href: "#florist",
-            },
-            {
-              title: "Batch in the Coromandel",
-              href: "#batch1",
-            },
-            {
-              title: "School in Taranaki",
-              href: "#school1",
-            },
-            {
-              title: "Commercial space",
-              href: "#commercial",
-            },
-            {
-              title: "Grahamstown Florist",
-              href: "#florist",
-            },
-            {
-              title: "Batch in the Coromandel",
-              href: "#batch1",
-            },
-            {
-              title: "School in Taranaki",
-              href: "#school1",
-            },
-            {
-              title: "Commercial space",
-              href: "#commercial",
-            },
-            {
-              title: "Grahamstown Florist",
-              href: "#florist",
-            },
-            {
-              title: "Batch in the Coromandel",
-              href: "#batch1",
-            },
-            {
-              title: "School in Taranaki",
-              href: "#school1",
-            },
-            {
-              title: "Commercial space",
-              href: "#commercial",
-            },
-          ].map((line) => (
-            <CustomLink href={line.href}>{line.title}</CustomLink>
+          {projectsList.map((line) => (
+            <CustomLink href={line.id}>{line.title}</CustomLink>
           ))}
         </div>
         <div>
           <p className="opacity-60">projects by type</p>
-          <p>Residential</p>
-          <p>Commercial</p>
-          <p>Schools</p>
+          <div className="flex flex-col gap-1">
+            <CustomLink onClick={() => setSelectedType("")}>All</CustomLink>
+            <CustomLink onClick={() => setSelectedType(Types.Residential)}>
+              Residential
+            </CustomLink>
+            <CustomLink onClick={() => setSelectedType(Types.Commercial)}>
+              Commercial
+            </CustomLink>
+            <CustomLink onClick={() => setSelectedType(Types.School)}>
+              Schools
+            </CustomLink>
+          </div>
         </div>
       </div>
       <div className="flex flex-col  flex-1">
         <PageHeader title="Projects"></PageHeader>
         <div className="flex flex-col gap-6 py-6">
-          <Tile
-            title="Grahamstown Florist"
-            image="/projects/Project_other2.png"
-          />
-          <Tile
-            title="Batch in the Coromandel"
-            image="/projects/Project_08.png"
-          />
-          <Tile
-            id="#batch"
-            title="Grahamstown Florist"
-            image="/projects/Project_12.png"
-          />
-          <Tile
-            id="#school1"
-            title="School in Taranaki"
-            image="/projects/Project_14.png"
-          />
-          <Tile
-            id="#commercial"
-            title="Commercial space"
-            image="/projects/Project_15.png"
-          />
-          <Tile title="Grahamstown Florist" image="/projects/Project_24.png" />
-          <Tile title="Grahamstown Florist" image="/projects/Project_30.png" />
-          <Tile title="Grahamstown Florist" image="/projects/Project_39.png" />
-          <Tile title="Grahamstown Florist" image="/projects/Project_129.png" />
-          <Tile
-            title="Grahamstown Florist"
-            image="/projects/Project_other.png"
-          />
-          <Tile
-            title="Grahamstown Florist"
-            image="/projects/Project_other.png"
-          />
-          <Tile
-            title="Grahamstown Florist"
-            image="/projects/Project_other3.png"
-          />
-          <Tile
-            title="Grahamstown Florist"
-            image="/projects/Project_other4.png"
-          />
+          {projects.map((item) => (
+            <Tile id={item.id} title={item.title} image={item.image} />
+          ))}
         </div>
       </div>
       <div className="flex-1"></div>
--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 
 const CustomLink = ({
-  href,
+  href = "",
+  onClick,
   children,
 }: {
-  href: string;
+  href?: string;
+  onClick?: () => void;
   children: React.ReactNode;
 }) => (
   <Link
     className="hover:opacity-90 hover:underline hover:underline-offset-4 decoration-2"
     href={href}
+    onClick={onClick}
   >
     {children}
   </Link>
