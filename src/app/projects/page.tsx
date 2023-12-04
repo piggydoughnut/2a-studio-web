66ed309e3865cee22ad6cac94b1727a3f297fa11
piggydoughnut <piggydoughnut@gmail.com>
2023-12-04 13:19:03 +0800

    named the projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,17 +10,19 @@ import Par from "@/components/Par";
 const Types = {
   Commercial: "Commercial",
   Residential: "Residential",
-  School: "School",
+  Educational: "Educational",
 };
 
 const Tile = ({
   image,
   title,
   id,
+  selected,
 }: {
   image: string;
   title: string;
   id: string;
+  selected: boolean;
 }) => (
   <div id={id} className="">
     <ProjectTitle title={title} />
@@ -29,87 +31,86 @@ const Tile = ({
       alt={image}
       width="400"
       height="220"
-      className="grayscale hover:grayscale-0"
+      className={`grayscale hover:grayscale-0 ${selected ? "grayscale-0" : ""}`}
     />
   </div>
 );
 
 const projectsList = [
   {
-    id: "#batch",
-    title: "Grahamstown Florist",
+    id: "1_container_florist",
+    title: "Container Florist",
     type: Types.Commercial,
-    image: "/projects/Project_other2.png",
+    image: "/projects/1_container_florist.png",
   },
   {
-    id: "#batch",
-    title: "Batch in the Coromandel",
+    id: "2_townhouse_3bdr",
+    title: "Townhouse with 3 bedrooms",
     type: Types.Residential,
-    image: "/projects/Project_08.png",
+    image: "/projects/2_townhouse_3bdr.png",
   },
   {
-    id: "#batch",
-    title: "Coromandel Town School",
-    type: Types.School,
-    image: "/projects/Project_12.png",
-  },
-  {
-    id: "#batch",
-    title: "43 Brown street, Thames",
+    id: "3_thames_coast_reno",
+    title: "Thames coast renovation",
     type: Types.Residential,
-    image: "/projects/Project_14.png",
+    image: "/projects/3_thames_coast_reno.png",
   },
   {
-    id: "#batch",
-    title: "Beach house",
+    id: "4_3bdr_family_home",
+    title: "3 bedroom family home with minor unit",
     type: Types.Residential,
-    image: "/projects/Project_15.png",
+    image: "/projects/4_3bdr_family_home.png",
+  },
+  {
+    id: "5_school_classroom",
+    title: "School classroom",
+    type: Types.Educational,
+    image: "/projects/5_school_classroom.png",
   },
 
   {
-    id: "#batch",
-    title: "Taranaki school",
-    type: Types.School,
-    image: "/projects/Project_24.png",
+    id: "6_4bdr_house_thames_coast",
+    title: "4 bedroom Thames coast house",
+    type: Types.Residential,
+    image: "/projects/6_4bdr_house_thames_coast.png",
   },
 
   {
-    id: "#batch",
-    title: "Auckland beach house",
-    type: Types.Commercial,
-    image: "/projects/Project_30.png",
+    id: "7_tiny_house",
+    title: "Tiny house ",
+    type: Types.Residential,
+    image: "/projects/7_tiny_house.png",
   },
 
   {
-    id: "#batch",
-    title: "Grahamstown Florist",
+    id: "8_kitchen_renovation",
+    title: "Kitchen Renovation",
     type: Types.Residential,
-    image: "/projects/Project_39.png",
+    image: "/projects/8_kitchen_renovation.png",
   },
   {
-    id: "#batch",
-    title: "School in the Coromandel",
-    type: Types.School,
-    image: "/projects/Project_129.png",
+    id: "9_school_refurbishment",
+    title: "School refurbishment ",
+    type: Types.Educational,
+    image: "/projects/9_school_refurbishment.png",
   },
   {
-    id: "#batch",
-    title: "School in Waihi",
+    id: "10_3bdrm_home_minor_unit",
+    title: "3 bedroom family home with minor unit",
     type: Types.Residential,
-    image: "/projects/Project_other.png",
+    image: "/projects/10_3bdrm_home_minor_unit.png",
   },
   {
-    id: "#batch",
-    title: "Batch in the Coromandel",
+    id: "11_school_amphitheatre",
+    title: "School refurbishment and amphitheatre",
+    type: Types.Educational,
+    image: "/projects/11_school_amphitheatre.png",
+  },
+  {
+    id: "12_1bdr_townhouse",
+    title: "1 bedroom townhouse",
     type: Types.Residential,
-    image: "/projects/Project_other3.png",
-  },
-
-  {
-    id: "#batch",
-    title: "Waihi beach school",
-    type: Types.Commercial,
-    image: "/projects/Project_other4.png",
+    image: "/projects/12_1bdr_townhouse.png",
   },
 ];
 
@@ -126,6 +127,7 @@ const ProjectTitle = ({ title }: { title: string }) => {
 export default function Home() {
   const [projects, setProjects] = useState(projectsList);
   const [selectedType, setSelectedType] = useState("");
+  const [selectedId, setSelectedId] = useState("");
 
   useEffect(() => {
     setProjects(projectsList.filter((p) => p.type === selectedType));
@@ -139,21 +141,59 @@ export default function Home() {
         <div className="flex flex-col gap-1">
           <p className="opacity-60 text-[14px]">projects by name</p>
           {projectsList.map((line) => (
-            <CustomLink href={line.id}>{line.title}</CustomLink>
+            <CustomLink
+              key={line.id}
+              selected={selectedId === line.id}
+              onClick={() => {
+                setSelectedId(line.id);
+                setSelectedType("");
+              }}
+              href={`#${line.id}`}
+            >
+              {line.title}
+            </CustomLink>
           ))}
         </div>
         <div>
           <p className="opacity-60 text-[14px]">projects by type</p>
           <div className="flex flex-col gap-1">
-            <CustomLink onClick={() => setSelectedType("")}>All</CustomLink>
-            <CustomLink onClick={() => setSelectedType(Types.Residential)}>
-              Residential
+            <CustomLink
+              selected={selectedType === ""}
+              onClick={() => {
+                setSelectedType("");
+                setSelectedId("");
+              }}
+            >
+              All
             </CustomLink>
-            <CustomLink onClick={() => setSelectedType(Types.Commercial)}>
-              Commercial
+
+            <CustomLink
+              selected={selectedType === Types.Residential}
+              onClick={() => {
+                setSelectedType(Types.Residential);
+                setSelectedId("");
+              }}
+            >
+              {Types.Residential}
             </CustomLink>
-            <CustomLink onClick={() => setSelectedType(Types.School)}>
-              Schools
+
+            <CustomLink
+              selected={selectedType === Types.Commercial}
+              onClick={() => {
+                setSelectedType(Types.Commercial);
+                setSelectedId("");
+              }}
+            >
+              {Types.Commercial}
+            </CustomLink>
+            <CustomLink
+              selected={selectedType === Types.Educational}
+              onClick={() => {
+                setSelectedType(Types.Educational);
+                setSelectedId("");
+              }}
+            >
+              {Types.Educational}
             </CustomLink>
           </div>
         </div>
@@ -162,7 +202,13 @@ export default function Home() {
         <PageHeader title="Projects"></PageHeader>
         <div className="flex flex-col gap-6 py-6">
           {projects.map((item) => (
-            <Tile id={item.id} title={item.title} image={item.image} />
+            <Tile
+              key={item.id}
+              selected={selectedId === item.id}
+              id={item.id}
+              title={item.title}
+              image={item.image}
+            />
           ))}
         </div>
       </div>
--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -2,15 +2,19 @@ import Link from "next/link";
 
 const CustomLink = ({
   href = "",
+  selected = false,
   onClick,
   children,
 }: {
   href?: string;
+  selected?: boolean;
   onClick?: () => void;
   children: React.ReactNode;
 }) => (
   <Link
-    className="hover:opacity-90 hover:underline hover:underline-offset-4 decoration-2"
+    className={`hover:opacity-90 hover:underline hover:underline-offset-4 decoration-2 ${
+      selected ? "text-orange-500" : ""
+    }`}
     href={href}
     onClick={onClick}
   >
